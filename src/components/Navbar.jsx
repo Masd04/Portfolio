afea29f5f658be70f5d34d3302f4a6d288e2a2d2
David Masek <masd04@vse.cz>
2023-08-21 00:06:21 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,13 +9,13 @@ function Navbar() {
   return (
     <div className="fixed left-0 right-0 top-0 h-20 shadow-md border-b-2 border-gray-100 bg-primary z-10">
 
-      <nav className="flex items-center mx-10 h-full justify-between">
+      <nav className="flex items-center ml-6 sm:ml-10 h-full justify-center sm:justify-between">
         
-        <img src={logo} alt="Ma.Dev logo" className="w-[18%]"/>
+        <img src={logo} alt="Ma.Dev logo" className="w-[75%] sm:w-[18%]"/>
         
 
         <div>
-          <ul className="flex items-center space-x-10 text-sm">
+          <ul className="hidden sm:flex items-center sm:mr-20 space-x-10 text-sm">
             <li><Link to="/" className="text-white hover:text-yellow-400">Home</Link></li>
             <li><Link to="/about" className="text-white hover:text-yellow-400">About Us</Link></li>
             <li><Link to="/docs" className="text-white hover:text-yellow-400">Docs</Link></li>
